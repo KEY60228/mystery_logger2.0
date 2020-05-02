53b3df6fa4478ee99451660e33fa53c5e7b190a0
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-02 21:52:28 +0900

    update CSS for layout.php (next: for home)

--- views/layout.php
+++ views/layout.php
@@ -2,49 +2,58 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="/css/style.css">
   <title><?php if (isset($title)) {echo $this->escape($title) . ' - ';} ?>なぞログ</title>
 </head>
 <body>
+
   <header>
-    <h1 class="header-logo">
+    <p>
       <?php if (!$session->isAuthenticated()) : ?>
         <a href="<?php echo $base_url; ?>/">なぞログ</a>
       <?php else : ?>
         <a href="<?php echo $base_url ;?>/posts">なぞログ</a>
       <?php endif; ?>
-    </h1>
-    <ul class="header-menus">
-      <?php if ($session->isAuthenticated()) : ?>
-        <li>
-          <!-- もっと良い書き方あるだろうけどとりあえずね… -->
-          <a href="<?php echo $base_url; ?>/users/<?php $user = $session->get('user'); echo $user['id']; ?>"><?php echo $user['name'] ?></a>
-        </li>
-        <li>
-          <a href="<?php echo $base_url; ?>/performances">公演一覧</a>
-        </li>
-        <li>
-          <a href="<?php echo $base_url; ?>/posts/new">新規投稿</a>
-        </li>
-        <li>
-          <a href="<?php echo $base_url; ?>/users/signout">ログアウト</a>
-        </li>
-      <?php else: ?>
-        <li>
-          <a href="<?php echo $base_url; ?>/users/signin">ログイン</a>
-        </li>
-        <li>
-          <a href="<?php echo $base_url; ?>/users/signup">新規登録</a>
-        </li>
-        <li>
-          <a href="<?php echo $base_url; ?>/about">なぞログ #とは</a>
-        </li>
-      <?php endif; ?>
-    </ul>
+    </p>
+    <nav>
+      <ul>
+        <?php if ($session->isAuthenticated()) : ?>
+          <?php $user = $session->get('user'); ?>
+          <li>
+            <a href="<?php echo $base_url; ?>/users/<?php echo $user['id']; ?>"><?php echo $user['name'] ?></a>
+          </li>
+          <li>
+            <a href="<?php echo $base_url; ?>/performances">公演一覧</a>
+          </li>
+          <li>
+            <a href="<?php echo $base_url; ?>/posts/new">新規投稿</a>
+          </li>
+          <li>
+            <a href="<?php echo $base_url; ?>/users/signout">ログアウト</a>
+          </li>
+        <?php else: ?>
+          <li>
+            <a href="<?php echo $base_url; ?>/users/signin">ログイン</a>
+          </li>
+          <li>
+            <a href="<?php echo $base_url; ?>/users/signup">新規登録</a>
+          </li>
+          <li>
+            <a href="<?php echo $base_url; ?>/about">なぞログ #とは</a>
+          </li>
+        <?php endif; ?>
+      </ul>
+    </nav>
   </header>
 
   <main>
     <?php echo $_content; ?>
   <main>
+
+  <footer>
+    <p> - なぞログ - </p>
+    <p>Copyright <a href="https://twitter.com/tatsuya_gucci" target=”_blank”>@tatsuya_gucci</a></p>
+  </footer>
 
 </body>
 </html>
