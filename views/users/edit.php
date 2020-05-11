0cdeda1aa88537693121ad74cb669e4a4fbfc25c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-11 21:54:28 +0900

    finish designs (next: JS)

--- views/users/edit.php
+++ views/users/edit.php
@@ -1,32 +1,27 @@
 <?php $this->setLayoutVar('title', '編集') ?>
 
-<h2>ユーザー情報の編集</h2>
+<div class="users-edit-wrapper">
+  <div class="users-edit-container">
+    <h2>ユーザー情報の編集</h2>
+    
+    <form action="<?php echo $base_url; ?>/users/update" method="post" enctype="multipart/form-data" class="users-edit-form">
+      <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
+      
+      <?php if (isset($errors) && count($errors) > 0): ?>
+        <?php echo $this->render('errors', array('errors' => $errors)); ?>
+      <?php endif; ?>
 
-<form action="<?php echo $base_url; ?>/users/update" method="post" enctype="multipart/form-data">
-  <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
+      <div class="users-edit-content">
+        <p>ユーザー名</p>
+        <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>">
+      </div>
+      <div class="users-edit-img">
+        <p>プロフィール画像 (jpeg, pngのみ)</p>
+        <input type="file" name="profile_image">
+      </div>
+    
+      <input type="submit" value="保存" class="users-edit-button">
 
-  <?php if (isset($errors) && count($errors) > 0): ?>
-    <?php echo $this->render('errors', array('errors' => $errors)); ?>
-  <?php endif; ?>
-
-  <table>
-    <tbody>
-      <tr>
-        <th>ユーザー名</th>
-        <td>
-          <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>">
-        </td>
-      </tr>
-      <tr>
-        <th>プロフィール画像 (jpeg, pngのみ)</th>
-        <td>
-          <input type="file" name="profile_image">
-        </td>
-      </tr>
-    </tbody>
-  </table>
-
-  <p>
-    <input type="submit" value="保存">
-  </p>
-</form>
+    </form>
+  </div>
+</div>
--- views/users/followers.php
+++ views/users/followers.php
@@ -1,6 +1,10 @@
 <?php $this->setLayoutVar('title', 'フォロワー'); ?>
 
-<?php foreach ($followers as $follower): ?>
-  <img src="/user_images/<?php echo $follower['image_name']; ?>">
-  <a href="<?php echo $base_url; ?>/users/<?php echo $follower['id']; ?>"><?php echo $follower['name']; ?></a>
-<?php endforeach; ?>
+<div class="follows-wrapper">  
+  <?php foreach ($followers as $follower): ?>
+    <div class="follows-container">
+      <img src="/user_images/<?php echo $follower['image_name']; ?>">
+      <a href="<?php echo $base_url; ?>/users/<?php echo $follower['id']; ?>"><?php echo $follower['name']; ?></a>
+    </div>
+  <?php endforeach; ?>
+</div>
