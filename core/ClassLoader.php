2c7e59b896e1ac20916c6a3536e27f8629297401
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-13 23:32:28 +0900

    amend comments on core/ClassLoader.php

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -4,7 +4,7 @@ class ClassLoader {
   protected $dirs;
 
   /**
-   * ClassLoaderクラスのloadClassメソッドがオートロード時に呼び出されるよう登録する
+   * ClassLoader::loadClass()がオートロード時に呼び出されるよう登録する
    */
   public function register() {
     spl_autoload_register(array($this, 'loadClass'));
@@ -12,15 +12,19 @@ class ClassLoader {
 
   /**
    * オートロード時に探すディレクトリのフルパスを受け取り、登録する
+   * 
+   * @param string $dir
    */
   public function registerDir($dir) {
     $this->dirs[] = $dir;
   }
 
   /**
-   * オートロード時に呼び出されるメソッド
-   * クラス名を受け取り、対応するクラスファイルをClassLoaderクラスのregisterDirメソッドで
-   * 登録したディレクトリ配下から探し出し、requireする
+   * オートロードメソッド
+   * 
+   * 受け取ったクラス名をもつファイルを呼び出す
+   * 
+   * @param string $class
    */
   public function loadClass($class) {
     foreach ($this->dirs as $dir) {
