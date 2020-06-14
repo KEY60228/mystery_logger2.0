859cc5b72d11d3b0ad2c5900062460f399db3142
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-14 21:51:16 +0900

    amend comments on core/NoRightActionException.php

--- core/NoRightActionException.php
+++ core/NoRightActionException.php
@@ -1,8 +1,11 @@
 <?php
 
 /**
+ * 例外の定義
+ * 
  * なぞログ用に追加した例外処理
- * 今んとこPostControllerにしか使えない
+ * 権限がないにも関わらずアクセスを試みた時の例外 (ex. 投稿内容の編集 等)
+ * 今のところPostControllerにしか使えない
  */
 class NoRightActionException extends Exception {
 
