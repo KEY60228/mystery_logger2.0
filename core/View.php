4be99373b80e6c3a0139cd7a9ebbc2f743f68ddb
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-09 21:57:38 +0900

    amend comments on core/View.php

--- core/View.php
+++ core/View.php
@@ -1,13 +1,20 @@
 <?php
 
+/**
+ * ビュー管理クラス View
+ */
 class View {
   protected $base_dir;
   protected $defaults;
   protected $layout_variables = array();
 
   /**
-   * viewsディレクトリとビューファイルにデフォルトで渡したい連想配列の値を受け取り、
-   * それぞれ格納するコンストラクタ
+   * コンストラクタ
+   * 
+   * viewsディレクトリとビューファイルにデフォルトで渡したい連想配列の値を受け取り、それぞれ格納する
+   * 
+   * @param string $base_dir
+   * @param array $defaults
    */
   public function __construct($base_dir, $defaults = array()) {
     $this->base_dir = $base_dir;
@@ -15,16 +22,25 @@ class View {
   }
 
   /**
-   * レイアウトに渡す変数と内容を受け取り、連想配列に格納する
+   * レイアウトに渡す変数と内容を受け取り、連想配列に格納するメソッド
+   * 
+   * @param string $name
+   * @param mixed $value
    */
   public function setLayoutVar($name, $value) {
     $this->layout_variables[$name] = $value;
   }
 
   /**
-   * ビューファイルへのパス、ビューファイルに渡す変数、読み込むレイアウトファイルを受け取り、
+   * ビューファイルをレンダリングするメソッド
+   * 
    * アウトプットバッファリングを用いて$contentにビューファイルを格納し、返す
    * レイアウトファイルの指定がある場合は再度実行し、返す
+   * 
+   * @param string $_path
+   * @param array $_variables
+   * @param mixed $_layout
+   * @return string
    */
   public function render($_path, $_variables = array(), $_layout = false) {
     $_file = $this->base_dir . '/' . $_path . '.php';
@@ -48,6 +64,9 @@ class View {
 
   /**
    * 文字列を受け取り、エスケープしたものを返す
+   * 
+   * @param string $string
+   * @return string
    */
   public function escape($string) {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
