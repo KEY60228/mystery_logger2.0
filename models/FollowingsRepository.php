8f865ed133fba10e3a0406240f9ac0f532569ea1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-14 21:56:05 +0900

    amend comments on models/FollowingsRepository.php

--- models/FollowingsRepository.php
+++ models/FollowingsRepository.php
@@ -1,8 +1,16 @@
 <?php
 
+/**
+ * Followingsテーブルの管理クラス FollowingsRepository
+ */
 class FollowingsRepository extends DbRepository {
   /**
-   * フォロー元のユーザーIDとフォロー先のユーザーIDを受け取り、DBにinsert文を実行する
+   * フォローするメソッド
+   * 
+   * フォロー元のIDとフォロー先のIDを受け取り、insert文を実行する
+   * 
+   * @param int $user_id
+   * @param int $following_id
    */
   public function insert($user_id, $following_id) {
     $now = new DateTime();
@@ -15,7 +23,12 @@ class FollowingsRepository extends DbRepository {
   }
 
   /**
-   * フォロー元のユーザーIDとフォロー先のユーザーIDを受け取り、DBにdelete文を実行する
+   * フォロー解除するメソッド
+   * 
+   * フォロー元のIDとフォロー先のIDを受け取り、delete文を実行する
+   * 
+   * @param int $user_id
+   * @param int $following_id
    */
   public function delete($user_id, $following_id) {
     $sql = "DELETE FROM followings WHERE user_id = :user_id AND following_id = :following_id";
@@ -26,9 +39,14 @@ class FollowingsRepository extends DbRepository {
   }
 
   /**
+   * フォローしているか確認するメソッド
+   * 
    * user_idとfollowing_idを受け取り、DBにセレクト文を実行、
    * 一致するuser_idとfollowing_idが0でなければtrue、それ以外ならばfalseを返す
-   * (MySQLなら文字列で、pgsqlなら整数型で返ってくるようなので型を含めない比較にした)
+   * 
+   * @param int $user_id
+   * @param int $following_id
+   * @return boolean
    */
   public function isFollowing($user_id, $following_id) {
     $sql = "SELECT COUNT(user_id) as count FROM followings WHERE user_id = :user_id AND following_id = :following_id";
@@ -45,7 +63,12 @@ class FollowingsRepository extends DbRepository {
   }
 
   /**
+   * フォロー数を返すメソッド
+   * 
    * ユーザーIDを受け取り、そのユーザーがフォローしてるユーザーの数を返す
+   * 
+   * @param int $user_id
+   * @return int
    */
   public function CountFollowingsByUserId($user_id) {
     $sql = "SELECT COUNT(following_id) as count FROM followings WHERE user_id = :user_id";
@@ -56,7 +79,12 @@ class FollowingsRepository extends DbRepository {
   }
   
   /**
-   * ユーザーIDを受け取り、そのユーザーがフォローされているユーザーの数を返す
+   * フォロワー数を返すメソッド
+   * 
+   * ユーザーIDを受け取り、そのユーザーのフォローされているユーザーの数を返す
+   * 
+   * @param int $user_id
+   * @return int
    */
   public function CountFollowersByUserId($user_id) {
     $sql = "SELECT COUNT(following_id) as count FROM followings WHERE following_id = :user_id";
